cb3fc87a4903c3ccd8a2551c50384dbb5db4e978
zhuwenbing <zhuwb@joobank.com>
2021-05-10 11:13:40 +0800

    Use 'gosu' instead of the command 'su' to run mongod by the user named 'mongodb'.

--- mongo/4.2/run.sh
+++ mongo/4.2/run.sh
@@ -7,4 +7,4 @@
 
 # Drop root privilege (no way back), exec provided command as user mongodb
 cmd=exec; for i; do cmd="$cmd '$i'"; done
-exec su -s /bin/sh -c "$cmd" mongodb
+exec gosu mongodb /bin/sh -c "$cmd"
