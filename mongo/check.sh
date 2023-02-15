cf615c3e6812522ce1383c27d54a90c1704c01bf
zhuwenbing <zhuwenbing@msn.cn>
2023-02-15 12:59:32 +0800

    Fix the problem that there is no mongo command in MongoDB 6.0 and above.

--- mongo/check.sh
+++ mongo/check.sh
@@ -3,8 +3,14 @@ set -eo pipefail
 
 host="$(hostname --ip-address || echo '127.0.0.1')"
 
-if mongo --quiet "$host/test" --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)'; then
-  exit 0
+if [[ -n $( command -v mongo ) ]]; then
+  if mongo --quiet "$host/test" --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)'; then
+    exit 0
+  fi
+elif [[ -n $( command -v mongosh ) ]]; then
+  if mongosh --quiet "$host/test" --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)'; then
+    exit 0
+  fi
 fi
 
 exit 1
