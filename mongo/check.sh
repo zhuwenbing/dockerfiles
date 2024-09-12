2375cb7c637bb6b42ce39ed44185329c5fcd3761
KissIce Chu <zhuwenbing@msn.cn>
2024-09-12 10:46:15 +0800

    Update check.sh
    Loops through mongo and mongosh commands, reducing redundancy.

--- mongo/check.sh
+++ mongo/check.sh
@@ -3,14 +3,12 @@ set -eo pipefail
 
 host="$(hostname --ip-address || echo '127.0.0.1')"
 
-if [[ -n $( command -v mongo ) ]]; then
-  if mongo --quiet "$host/test" --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)'; then
-    exit 0
+for cmd in mongo mongosh; do
+  if command -v "$cmd" >/dev/null; then
+    if "$cmd" --quiet "$host/test" --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)'; then
+      exit 0
+    fi
   fi
-elif [[ -n $( command -v mongosh ) ]]; then
-  if mongosh --quiet "$host/test" --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)'; then
-    exit 0
-  fi
-fi
+done
 
 exit 1
